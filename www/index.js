e138ac9c2948b5ff8890511e21176bae5a6e2b7d
probertr-dc <64196168+probertr-dc@users.noreply.github.com>
2020-09-15 12:59:36 +0100

    Added login screen

--- www/index.js
+++ www/index.js
@@ -1,4 +1,14 @@
 function CheckAnswer() {
     ans.textContent = "800 nautical miles";
 }
-document.getElementById("x").onclick = CheckAnswer;
+function Login() {
+    var uName = document.getElementById("username");
+    var pWord = document.getElementById("password");
+    if (uName.value == "Rory" && pWord.value == "123456") {
+        ons.notification.alert("Login Successful");
+    }
+    else {
+        ons.notification.alert("Login Failed");
+    }
+}
+document.getElementById("signIn").onclick = Login;
