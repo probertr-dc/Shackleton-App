ee6059830f75bc0e43d7d240fe3398f26eada8c2
probertr-dc <64196168+probertr-dc@users.noreply.github.com>
2020-09-15 13:08:43 +0100

    test

--- www/index.js
+++ www/index.js
@@ -2,6 +2,11 @@ function CheckAnswer() {
     ans.textContent = "800 nautical miles";
 }
 function Login() {
-    ons.notification.alert("Hello");
+    if (document.getElementById("username").value == "Rory" && document.getElementById("password").value == "1233456") {
+        ons.notification.alert("Login Successful");
+    }
+    else {
+        ons.notification.alert("Login Failed");
+    }
 }
 document.getElementById("signIn").onclick = Login;
