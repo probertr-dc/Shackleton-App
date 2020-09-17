28abc890c0cfc8a130fd735de7f7885e8e32a4db
probertr-dc <64196168+probertr-dc@users.noreply.github.com>
2020-09-17 11:36:16 +0100

    Update index.js

--- www/index.js
+++ www/index.js
@@ -2,9 +2,9 @@ function CheckAnswer() {
     ans.textContent = "800 nautical miles";
 }
 function Login() {
-    if (document.getElementById("username").value == "Rory" && document.getElementById("password").value == "1233456") {
+    if (document.getElementById("username").value == "Rory" && document.getElementById("password").value == "123456") {
         var navigator = document.getElementById("navigator");
-        navigator.push_page("shackleton.html");
+        navigator.resetToPage("shackleton.html");
     }
     else {
         ons.notification.alert("Login Failed");
