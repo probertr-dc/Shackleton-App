b99c960d50d3cdf116b10fb24e3bb23557fb244d
probertr-dc <64196168+probertr-dc@users.noreply.github.com>
2020-10-15 12:05:14 +0100

    db

--- www/login.js
+++ www/login.js
@@ -1,9 +1,26 @@
-function Login() {
-    if (document.getElementById("username").value == "Rory" && document.getElementById("password").value == "123456") {
-        var navigator = document.getElementById("navigator");
-        navigator.resetToPage("shackleton.html");
-    }
-    else {
-        ons.notification.alert("Login Failed");
-    }
+function login() {
+    // Get username and password entered by the user
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Connect to firebase and look at the “users” collection
+    var ref = firebase.database().ref("users");
+    // see if there is a user with same id as entered
+    ref.child(username).once('value', function (snapshot) {
+        var exists = (snapshot.val() !== null);
+        if (exists) {
+            // if user exists then check password
+            if (snapshot.val().pass == password) {
+                // if password is correct then load shackleton.html page
+                var navigator = document.getElementById('navigator');
+                navigator.resetToPage('shackleton.html');
+            }
+            else {
+                ons.notification.alert('Incorrect passowrd');
+            }
+        }
+        else {
+            ons.notification.alert('Username unknown');
+        }
+    })
 }
